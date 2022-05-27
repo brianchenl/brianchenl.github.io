5967f09862435498e577ca99a3a4b281bec537de
chenliang <chenliang@idcos.com>
2022-05-27 22:47:57 +0800

    Site updated: 2022-05-27 22:47:57

---
+++ slides/reveal.js
@@ -0,0 +1,12 @@
+slide:
+  separator: ---
+  separator_vertical: --
+  charset: utf-8
+  theme: black
+  mouseWheel: false
+  transition: slide
+  transitionSpeed: default
+  parallaxBackgroundImage: ''
+  parallaxBackgroundSize: ''
+  parallaxBackgroundHorizontal: null
+  parallaxBackgroundVertical: null
